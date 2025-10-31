d240f7f8c85020605f3a71bc86b731ee99505548
LH-and-FPGA <180024497+LH-and-FPGA@users.noreply.github.com>
2025-10-31 10:48:46 +0000

    Task1 Done

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -4,5 +4,15 @@ module lfsr(
     input   logic       en,
     output  logic [3:0] data_out
 );
+    logic [3:0] sreg;
+    always_ff @(posedge clk, posedge rst) begin
+        if (rst)
+            sreg <= 4'b1; // 需要设置en吗
+        else begin
+           if (en)
+                sreg <= {sreg[2:0], sreg[3]^sreg[2]}; 
+        end
+    end
+    assign data_out = sreg;
 
 endmodule
--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -4,5 +4,14 @@ module lfsr_7 (
     input   logic       en,
     output  logic [6:0] data_out
 );
-
+    logic [6:0] sreg;
+    always_ff @(posedge clk, posedge rst) begin
+        if (rst)
+            sreg <= 7'b1;
+        else begin
+           if (en)
+                sreg <= {sreg[5:0], sreg[6]^sreg[2]}; 
+        end
+    end
+    assign data_out = sreg;
 endmodule
