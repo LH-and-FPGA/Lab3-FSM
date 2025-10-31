da3ed0ac18af84ec9f0d98c0681815888ba2049d
LH-and-FPGA <180024497+LH-and-FPGA@users.noreply.github.com>
2025-10-31 11:24:39 +0000

    Task2 ongoing

--- task2/f1_fsm.sv
+++ task2/f1_fsm.sv
@@ -4,5 +4,39 @@ module f1_fsm (
     input   logic       clk,
     output  logic [7:0] data_out
 );
+    typedef enum logic [7:0] {
+        S0=8'b0, 
+        S1=8'b1, 
+        S2=8'b11,
+        S3=8'b111,
+        S4=8'b1111,
+        S5=8'b11111,
+        S6=8'b111111,
+        S7=8'b1111111,
+        S8=8'b11111111} light;
+    light current, next;
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) current <= S0;
+        else if (en) begin
+            current <= next;
+        end
+    end
+
+    always_comb begin
+        case (current)
+            S0: next = S1;
+            S1: next = S2;
+            S2: next = S3;
+            S3: next = S4;
+            S4: next = S5;
+            S5: next = S6;
+            S6: next = S7;
+            S7: next = S8;
+            S8: next = S0;
+            default: next = S0;
+        endcase
+    end
+
+    assign data_out = current;
 
 endmodule
---
+++ task2/f1_fsm_tb.cpp
@@ -0,0 +1,54 @@
+#include "Vf1_fsm.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+    Verilated::commandArgs(argc, argv);
+    // TODO: Check the doc about this
+
+    Vf1_fsm* f1_fsm = new Vf1_fsm;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    f1_fsm->trace(tfp, 99);
+    tfp->open("sinegen.vcd");
+
+    // init vbuddy
+    if (vbdOpen() != 1) return -1;
+    vbdHeader("Lab 3: FSM");
+    vbdSetMode(1);
+
+    f1_fsm->clk = 0;
+    f1_fsm->rst = 0;
+    f1_fsm->en = 0;
+
+    while(true) {
+        // sinegen->incr = vbdValue();
+        // 这个更加有趣一点儿
+        if (vbdFlag() == 1) {
+            f1_fsm->en = !f1_fsm->en;
+        }
+
+        vbdBar(f1_fsm->data_out & 0xFF);
+
+        for (clk = 0; clk < 2; clk++) {
+            // tfp->dump(2*i+clk);
+            f1_fsm->clk = !f1_fsm->clk;
+            f1_fsm->eval();
+        }
+
+        if (vbdGetkey() == 'q') {
+            vbdClose();
+            tfp->close();
+            exit(0);
+        }
+    }
+
+    vbdClose();
+    tfp->close();
+    exit(0);
+}
